d0997bf891b6bf61b0cc3faec56ea305d7808292
Boris Cabrera <boro101094@gmail.com>
2024-05-11 20:30:54 -0400

    creating initial plugin sctructure (files, folders)

---
+++ commitHistory.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Plugin Name: commitHistory
+* Plugin URI: https://www.test.com/
+* Description: List commit history of a repository from GitHub
+* Version: 0.1
+* Author: Boris Cabrera
+* Author URI: https://www.test.com/
+**/
+
